a66f10bc711fd06b88918d55a9e7e9179aadc03f
ツクモ <ryunomeiru@gmail.com>
2013-11-05 15:06:06 -0500

    changes to string library

--- lib/tsustr.h
+++ lib/tsustr.h
@@ -10,8 +10,27 @@
 
 __BEGIN_DECLS
 
+int 	tsu_str_index		(char*,char);
+int 	tsu_str_rindex		(char*,char);
+int 	tsu_str_len			(char*);
+int 	tsu_str_rev			(char*);
 
+char* 	tsu_str_cat			(char*,char*);
+int		tsu_str_cmp			(char*,char*);
+int		tsu_str_ncmp		(char*,char*,int);
+char*	tsu_str_concat		(char*,char*);
+char*	tsu_str_nconcat		(char*,char*,int);
+char*	tsu_str_cpy			(char*,char*);
+char*	tsu_str_ncpy		(char*,char*,int);
+char*	tsu_str_dup			(char*);
+
+char*	tsu_str_combine		(char**,char*);
+char** 	tsu_str_split		(char*,char);
+
+int 	tsu_atoi			(char*);
+char*	tsu_itoa			(int);
 
 __END_DECLS
+
 
 #endif
